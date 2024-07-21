fdacb7fc0265d64228616c7a563c456fc0d81739
KOSASIH <kosasihg88@gmail.com>
2024-07-21 19:53:44 +0700

    Create chain.js

---
+++ core/sidra-chain/chain.js
@@ -0,0 +1,22 @@
+import { SIDRA_CHAIN_PROTOCOL_VERSION } from '../config/constants';
+
+class SidraChain {
+  constructor() {
+    this.chain = [];
+    this.currentBlock = null;
+  }
+
+  init() {
+    // Initialize the chain with a genesis block
+  }
+
+  createBlock() {
+    // Create a new block and add it to the chain
+  }
+
+  processTransaction() {
+    // Process a transaction and update the chain state
+  }
+}
+
+export default SidraChain;
