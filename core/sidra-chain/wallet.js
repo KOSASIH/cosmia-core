d50aeca1e85fadbe50afbdd185991fe5256cadfe
KOSASIH <kosasihg88@gmail.com>
2024-07-21 22:25:58 +0700

    Create wallet.js

---
+++ core/sidra-chain/wallet.js
@@ -0,0 +1,18 @@
+// wallet.js
+import * as Wallet from 'wallet';
+
+class SidraChainWallet {
+  constructor(privateKey) {
+    this.privateKey = privateKey;
+  }
+
+  getPublicKey() {
+    // Get the public key from the private key
+  }
+
+  signTransaction(transaction) {
+    // Sign the transaction with the private key
+  }
+}
+
+export default SidraChainWallet;
