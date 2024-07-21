1802b13de2a3b7b2b35a7a637c4455b7640c9c65
KOSASIH <kosasihg88@gmail.com>
2024-07-21 22:35:35 +0700

    Create computer-vision.js

---
+++ core/sidra-chain/computer-vision/computer-vision.js
@@ -0,0 +1,18 @@
+// computer-vision.js
+import * as CV from 'cv.js';
+
+class SidraChainComputerVision {
+  constructor() {
+    this.cvModel = new CV.Model();
+  }
+
+  analyzeImage(image) {
+    // Analyze an image using computer vision
+  }
+
+  detectFraudulentActivity(image) {
+    // Detect fraudulent activity using computer vision
+  }
+}
+
+export default SidraChainComputerVision;
