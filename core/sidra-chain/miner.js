418fbb6fd75196c7254c6a196d52da0111cdfb9b
KOSASIH <kosasihg88@gmail.com>
2024-07-21 22:25:29 +0700

    Create miner.js

---
+++ core/sidra-chain/miner.js
@@ -0,0 +1,14 @@
+// miner.js
+import * as Miner from 'iner';
+
+class SidraChainMiner {
+  constructor(node) {
+    this.node = node;
+  }
+
+  mineBlock(transactions) {
+    // Mine a new block with the given transactions
+  }
+}
+
+export default SidraChainMiner;
