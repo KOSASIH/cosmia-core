a6e40e808873f0ce5f7848b08bdfe2213418c834
KOSASIH <kosasihg88@gmail.com>
2024-07-21 22:30:23 +0700

    Create iot.js

---
+++ core/sidra-chain/iot/iot.js
@@ -0,0 +1,18 @@
+// iot.js
+import * as IoT from 'iot';
+
+class SidraChainIoT {
+  constructor() {
+    this.devices = [];
+  }
+
+  addDevice(device) {
+    // Add a new IoT device to the network
+  }
+
+  receiveDataFromDevice(device) {
+    // Receive data from an IoT device
+  }
+}
+
+export default SidraChainIoT;
