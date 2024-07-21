98ce36579771a2103e094d59a2aed31378041056
KOSASIH <kosasihg88@gmail.com>
2024-07-21 20:10:58 +0700

    Update transaction.js

--- core/sidra-chain/transaction.js
+++ core/sidra-chain/transaction.js
@@ -1,16 +1,18 @@
 class Transaction {
-  constructor(from, to, data) {
+  constructor(from, to, amount, data) {
     this.from = from;
     this.to = to;
+    this.amount = amount;
     this.data = data;
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    // Calculate the transaction's hash using a cryptographic algorithm
   }
 
   validate() {
-    // Validate the transaction and ensure it meets the protocol's requirements
-  }
-
-  process() {
-    // Process the transaction and update the chain state
+    // Validate the transaction's data and amounts
   }
 }
 
