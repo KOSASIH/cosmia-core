f3cebea6a6b009b6c300f49bf53fe2ed54b17707
KOSASIH <kosasihg88@gmail.com>
2024-07-21 22:28:49 +0700

    Create blockchain.js

---
+++ core/sidra-chain/blockchain/blockchain.js
@@ -0,0 +1,18 @@
+// blockchain.js
+import * as Blockchain from 'blockchain';
+
+class SidraChainBlockchain {
+  constructor() {
+    this.chain = [];
+  }
+
+  addBlock(block) {
+    // Add a new block to the blockchain
+  }
+
+  validateBlockchain() {
+    // Validate the entire blockchain
+  }
+}
+
+export default SidraChainBlockchain;
