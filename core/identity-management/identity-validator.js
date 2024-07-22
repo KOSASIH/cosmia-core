d95375f0e7cff5c09941024371e076c448b5a202
KOSASIH <kosasihg88@gmail.com>
2024-07-22 10:32:52 +0700

    Update identity-validator.js

--- core/identity-management/identity-validator.js
+++ core/identity-management/identity-validator.js
@@ -1,19 +1,27 @@
 // identity-validator.js
 import { Identity } from 'identity-sdk';
 import { Validator } from 'validator-sdk';
+import { KYC } from 'kyc-sdk';
 
 class IdentityValidator {
   constructor() {
     this.identity = new Identity();
     this.validator = new Validator();
+    this.kyc = new KYC();
   }
 
   validateIdentity(identity) {
     // Validate an identity using advanced validation algorithms
+    const kycResult = this.kyc.verify(identity);
+    if (!kycResult) {
+      throw new Error('KYC verification failed');
+    }
+    return this.validator.validate(identity);
   }
 
   verifyIdentityAttributes(attributes) {
     // Verify the attributes associated with an identity
+    return this.validator.verify(attributes);
   }
 }
 
