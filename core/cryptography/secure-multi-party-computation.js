23e163ebbfa118cf013a67cb166c3ea98c325432
KOSASIH <kosasihg88@gmail.com>
2024-07-22 10:31:06 +0700

    Update secure-multi-party-computation.js

--- core/cryptography/secure-multi-party-computation.js
+++ core/cryptography/secure-multi-party-computation.js
@@ -1,17 +1,26 @@
 // secure-multi-party-computation.js
 import { SMPC } from 'mpc-sdk';
+import { Paillier } from 'paillier-sdk';
+import { ElGamal } from 'elgamal-sdk';
 
 class SecureMultiPartyComputation {
   constructor() {
     this.smpc = new SMPC();
+    this.paillier = new Paillier();
+    this.elGamal = new ElGamal();
   }
 
   computeFunction(function, inputs) {
     // Compute a function on private inputs from multiple parties securely
+    const encryptedInputs = inputs.map((input) => this.paillier.encrypt(input));
+    const computedResult = this.smpc.compute(function, encryptedInputs);
+    return this.paillier.decrypt(computedResult);
   }
 
   shareResults(results) {
     // Share the results of the computation with the parties securely
+    const encryptedResults = results.map((result) => this.elGamal.encrypt(result));
+    return encryptedResults;
   }
 }
 
