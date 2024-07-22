affc3a08cac6e573e92cdaa786fc876b93c238b7
KOSASIH <kosasihg88@gmail.com>
2024-07-22 10:33:37 +0700

    Update interoperability-protocol.js

--- core/interoperability/interoperability-protocol.js
+++ core/interoperability/interoperability-protocol.js
@@ -2,20 +2,49 @@
 import { Interoperability } from 'interoperability-sdk';
 import { Ethereum } from 'ethereumjs-util';
 import { Polkadot } from 'polkadot-sdk';
+import { BinanceSmartChain } from 'binance-smart-chain-sdk';
 
 class InteroperabilityProtocol {
   constructor() {
     this.interoperability = new Interoperability();
     this.ethereum = new Ethereum();
     this.polkadot = new Polkadot();
+    this.binanceSmartChain = new BinanceSmartChain();
   }
 
   establishConnection(chain) {
     // Establish a connection to an interoperability protocol
+    switch (chain) {
+      case 'ethereum':
+        return this.ethereum.connect();
+      case 'polkadot':
+        return this.polkadot.connect();
+      case 'binanceSmartChain':
+        return this.binanceSmartChain.connect();
+      default:
+        throw new Error('Unsupported blockchain type');
+    }
   }
 
   transferAssets(from, to, amount) {
     // Transfer assets between different blockchain networks using the interoperability protocol
+    const fromChain = this.getChain(from);
+    const toChain = this.getChain(to);
+    return this.interoperability.transfer(fromChain, toChain, amount);
+  }
+
+  getChain(chainId) {
+    // Get the blockchain instance based on the chain ID
+    switch (chainId) {
+      case 'ethereum':
+        return this.ethereum;
+      case 'polkadot':
+        return this.polkadot;
+      case 'binanceSmartChain':
+        return this.binanceSmartChain;
+      default:
+        throw new Error('Unsupported blockchain type');
+    }
   }
 }
 
