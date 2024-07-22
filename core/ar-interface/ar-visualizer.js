e1469dd1830215016137d9d81075dc0308edd298
KOSASIH <kosasihg88@gmail.com>
2024-07-22 10:29:18 +0700

    Update ar-visualizer.js

--- core/ar-interface/ar-visualizer.js
+++ core/ar-interface/ar-visualizer.js
@@ -1,19 +1,52 @@
 // ar-visualizer.js
 import { AR } from 'ar-sdk';
-import { Three } from 'threejs';
+import { Visualization } from 'visualization-sdk';
+import { NetworkTopology } from 'network-topology-sdk';
+import { TransactionFlow } from 'transaction-flow-sdk';
 
 class ARVisualizer {
   constructor() {
     this.ar = new AR();
-    this.three = new Three();
+    this.visualization = new Visualization();
+    this.networkTopology = new NetworkTopology();
+    this.transactionFlow = new TransactionFlow();
   }
 
-  visualizeBlockchainData(data) {
-    // Visualize blockchain data in augmented reality
+  visualizeNetworkTopology() {
+    // Visualize the network topology in augmented reality
+    const nodes = this.networkTopology.getNodes();
+    const edges = this.networkTopology.getEdges();
+    this.ar.createScene();
+    nodes.forEach((node) => {
+      this.ar.createNode(node.id, node.position);
+    });
+    edges.forEach((edge) => {
+      this.ar.createEdge(edge.from, edge.to);
+    });
+    this.ar.renderScene();
   }
 
-  trackTransaction(transaction) {
-    // Track a transaction in augmented reality
+  visualizeTransactionFlow() {
+    // Visualize the transaction flow in augmented reality
+    const transactions = this.transactionFlow.getTransactions();
+    this.ar.createScene();
+    transactions.forEach((transaction) => {
+      this.ar.createTransactionNode(transaction.id, transaction.position);
+      this.ar.createEdge(transaction.from, transaction.to);
+    });
+    this.ar.renderScene();
+  }
+
+  visualizeNodeDetails(nodeId) {
+    // Visualize node details in augmented reality
+    const node = this.networkTopology.getNode(nodeId);
+    this.ar.createNodeDetails(node);
+  }
+
+  visualizeTransactionDetails(transactionId) {
+    // Visualize transaction details in augmented reality
+    const transaction = this.transactionFlow.getTransaction(transactionId);
+    this.ar.createTransactionDetails(transaction);
   }
 }
 
