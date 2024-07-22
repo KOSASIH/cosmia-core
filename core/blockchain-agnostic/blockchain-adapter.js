0f57c0648b7c14507c1da3b5f870285efc793735
KOSASIH <kosasihg88@gmail.com>
2024-07-22 10:30:06 +0700

    Update blockchain-adapter.js

--- core/blockchain-agnostic/blockchain-adapter.js
+++ core/blockchain-agnostic/blockchain-adapter.js
@@ -2,20 +2,56 @@
 import { Blockchain } from 'blockchain-sdk';
 import { Ethereum } from 'ethereumjs-util';
 import { Polkadot } from 'polkadot-sdk';
+import { BinanceSmartChain } from 'binance-smart-chain-sdk';
 
 class BlockchainAdapter {
   constructor() {
     this.blockchain = new Blockchain();
     this.ethereum = new Ethereum();
     this.polkadot = new Polkadot();
+    this.binanceSmartChain = new BinanceSmartChain();
   }
 
-  connectToBlockchain(blockchain) {
+  connectToBlockchain(blockchainType) {
     // Connect to a blockchain network
+    switch (blockchainType) {
+      case 'ethereum':
+        return this.ethereum.connect();
+      case 'polkadot':
+        return this.polkadot.connect();
+      case 'binanceSmartChain':
+        return this.binanceSmartChain.connect();
+      default:
+        throw new Error('Unsupported blockchain type');
+    }
   }
 
-  executeSmartContract(contract) {
+  executeSmartContract(contract, blockchainType) {
     // Execute a smart contract on a blockchain network
+    switch (blockchainType) {
+      case 'ethereum':
+        return this.ethereum.executeContract(contract);
+      case 'polkadot':
+        return this.polkadot.executeContract(contract);
+      case 'binanceSmartChain':
+        return this.binanceSmartChain.executeContract(contract);
+      default:
+        throw new Error('Unsupported blockchain type');
+    }
+  }
+
+  getBlockchainInfo(blockchainType) {
+    // Get information about a blockchain network
+    switch (blockchainType) {
+      case 'ethereum':
+        return this.ethereum.getInfo();
+      case 'polkadot':
+        return this.polkadot.getInfo();
+      case 'binanceSmartChain':
+        return this.binanceSmartChain.getInfo();
+      default:
+        throw new Error('Unsupported blockchain type');
+    }
   }
 }
 
