8494a0016aa77f3f705548a2dd8d7705661c4304
KOSASIH <kosasihg88@gmail.com>
2024-07-22 10:32:14 +0700

    Update data-encryption.js

--- core/data-encryption/data-encryption.js
+++ core/data-encryption/data-encryption.js
@@ -1,17 +1,37 @@
 // data-encryption.js
 import { Encryption } from 'encryption-sdk';
+import { AES } from 'aes-sdk';
+import { RSA } from 'rsa-sdk';
 
 class DataEncryption {
   constructor() {
     this.encryption = new Encryption();
+    this.aes = new AES();
+    this.rsa = new RSA();
   }
 
-  encryptData(data) {
+  encryptData(data, encryptionType) {
     // Encrypt data using advanced encryption algorithms
+    switch (encryptionType) {
+      case 'aes':
+        return this.aes.encrypt(data);
+      case 'rsa':
+        return this.rsa.encrypt(data);
+      default:
+        throw new Error('Unsupported encryption type');
+    }
   }
 
-  decryptData(data) {
+  decryptData(data, encryptionType) {
     // Decrypt data using advanced encryption algorithms
+    switch (encryptionType) {
+      case 'aes':
+        return this.aes.decrypt(data);
+      case 'rsa':
+        return this.rsa.decrypt(data);
+      default:
+        throw new Error('Unsupported encryption type');
+    }
   }
 }
 
