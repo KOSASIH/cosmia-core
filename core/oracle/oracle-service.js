65974fc34053a8b5b229282212edb804a6ea8b6a
KOSASIH <kosasihg88@gmail.com>
2024-07-22 10:34:19 +0700

    Update oracle-service.js

--- core/oracle/oracle-service.js
+++ core/oracle/oracle-service.js
@@ -1,17 +1,26 @@
 // oracle-service.js
 import { Oracle } from 'oracle-sdk';
+import { DataProvider } from 'data-provider-sdk';
 
 class OracleService {
   constructor() {
     this.oracle = new Oracle();
+    this.dataProvider = new DataProvider();
   }
 
   provideData(data) {
     // Provide data to the oracle service
+    this.dataProvider.provideData(data);
   }
 
   retrieveData(query) {
     // Retrieve data from the oracle service
+    return this.oracle.retrieveData(query);
+  }
+
+  registerDataProvider(dataProvider) {
+    // Register a data provider with the oracle service
+    this.oracle.registerDataProvider(dataProvider);
   }
 }
 
